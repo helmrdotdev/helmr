35b72bffb141b96aa8e622180278d7f66ecd04e9
aktky <theaktky@gmail.com>
2026-06-02 11:25:35 +0900

    fold schedule declarations into initial schema

--- internal/db/schema/migrations/000003_deployment_task_schedules.down.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE deployment_tasks
-    DROP COLUMN IF EXISTS schedule_declarations;
--- internal/db/schema/migrations/000003_deployment_task_schedules.up.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE deployment_tasks
-    ADD COLUMN IF NOT EXISTS schedule_declarations JSONB NOT NULL DEFAULT '[]'::jsonb;
