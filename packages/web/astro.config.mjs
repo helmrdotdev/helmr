edade917b378cb409860fc8609b4261d0db68997
aktky <theaktky@gmail.com>
2026-06-06 17:06:51 +0900

    enable docs client navigation

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -2,6 +2,10 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://helmr.dev",
+  prefetch: {
+    prefetchAll: false,
+    defaultStrategy: "hover",
+  },
   markdown: {
     shikiConfig: {
       theme: "github-light",
